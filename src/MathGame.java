bcbb938e5a9dd7e577879ab3d5216ce69de696f7
strmarius <marius.smc0@gmail.com>
2023-03-17 11:41:19 +0200

    Basic Math game for kids with following features:
    - 2 random generated numbers and ask for result
    - Feedback based on result
    - Option to set maximum value for generated number (difficulty)
    
    _________________
    
    v1.01
    
    - Set focus for answerField
    - Use ENTER key as shortcut for submitButton
    - MaxNum is also max sum beside generated numbers (move to Random class from Math.random)
    - Condition Random generator not to give same numbers as last exercise
    
    v1.02
    
    - Set MenuBar
    - Add Config for MaxNumber + MinNumber + KeyStroke
    - Solve a bug (if you enter max value = 1 program crash)
    - Solve bound for random (EX: for max = 2, you get only 0+0, 1+0 and 0+1)

--- src/MathGame.java
+++ src/MathGame.java
@@ -7,26 +7,35 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Random;
 
-public class MathGame extends JFrame implements ActionListener {
+public class MathGame extends JFrame implements ActionListener{
     private JTextField answerField;
     private JLabel num1Label, num2Label, resultLabel, feedbackLabel;
+    private JMenuBar menuBar;
     private JButton submitButton, resetButton;
-    private int maxNum, num1, num2, result;
+    private int maxNum, minNum, num1, num2, result;
     Random random = new Random();
 
-    public JLabel getNum1Label() {
-        return num1Label;
+    public void setMaxNum(int maxNum) {
+        this.maxNum = maxNum;
     }
 
-    public JLabel getNum2Label() {
-        return num2Label;
+    public void setMinNum(int minNum) {
+        this.minNum = minNum;
+    }
+
+    public int getMaxNum() {
+        return maxNum;
+    }
+
+    public int getMinNum() {
+        return minNum;
     }
 
     public MathGame(int maxNum) {
 
         this.maxNum = maxNum;
-        num1 = random.nextInt(maxNum);
-        num2 = random.nextInt(maxNum - num1);
+        num1 = random.nextInt(minNum, maxNum);
+        num2 = random.nextInt(maxNum - num1 + 1);
         result = num1 + num2;
 
         setTitle("MaHAHAte");
@@ -37,6 +46,46 @@ public class MathGame extends JFrame implements ActionListener {
 
         JPanel gamePanel = new JPanel(new GridLayout(4, 3, 15, 15));
         gamePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        // create MenuBar
+        menuBar = new JMenuBar();
+        JMenu config = new JMenu("Configurare");
+        JMenuItem config1 = new JMenuItem("Setează numărul maxim  ");
+        JMenuItem config2 = new JMenuItem("Setează numărul minim  ");
+        JMenuItem config3 = new JMenuItem("Afișează rezultatul");
+
+        config.add(config1);
+        config.add(config2);
+        config.add(config3);
+        menuBar.add(config);
+        this.setJMenuBar(menuBar);
+
+        // set config1 (Seteaza numarul maxim)
+        config1.addActionListener(e -> {
+            setMaxNum(Integer.parseInt(JOptionPane.showInputDialog("Operațiile sunt generate de la " + getMinNum() + " la " +getMaxNum() +
+                    ".\n\nIntrodu valoarea nouă pentru maxim: ")));
+
+            while (getMaxNum() <= 1) {
+                JOptionPane.showMessageDialog(null, "Numărul trebuie să fie mai mare sau egal cu 2");
+                setMaxNum(Integer.parseInt(JOptionPane.showInputDialog("Introdu valoarea nouă pentru maxim: ")));
+            }
+        });
+        config1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F12, 0));
+        // set config2 (Seteaza numarul minim)
+        config2.addActionListener(e -> {
+            setMinNum(Integer.parseInt(JOptionPane.showInputDialog("Operațiile sunt generate de la " + getMinNum() + " la " +getMaxNum() +
+                    ".\n\nIntrodu valoarea nouă pentru minim: ")));
+
+            while (getMinNum() < 0 || getMinNum() > getMaxNum()) {
+                JOptionPane.showMessageDialog(null, "Numărul trebuie să fie pozitiv și mai mic decât " + getMaxNum());
+                setMinNum(Integer.parseInt(JOptionPane.showInputDialog("Introdu valoarea nouă pentru minim: ")));
+            }
+        });
+        config2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F11, 0));
+
+        // set config3 (Afiseaza rezultatul)
+        config3.addActionListener(e -> JOptionPane.showMessageDialog(null, "", "Glumă", JOptionPane.PLAIN_MESSAGE));
+
 
         num1Label = new JLabel("" + num1, JLabel.CENTER);
         num1Label.setFont(num1Label.getFont().deriveFont(24f));
@@ -54,10 +103,10 @@ public class MathGame extends JFrame implements ActionListener {
         equalsLabel.setFont(equalsLabel.getFont().deriveFont(24f));
         gamePanel.add(equalsLabel);
 
+    // create answer box
         answerField = new JTextField();
         answerField.setFont(answerField.getFont().deriveFont(24f));
         answerField.requestFocus();
-//        answerField.addActionListener(KeyEvent.VK_ENTER == 0 ? submitButton.addActionListener(this) : null);
         answerField.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
@@ -68,10 +117,10 @@ public class MathGame extends JFrame implements ActionListener {
         });
         gamePanel.add(answerField);
 
+    //create submit button
         submitButton = new JButton("Verifică");
         submitButton.setFont(submitButton.getFont().deriveFont(24f));
         submitButton.addActionListener(this);
-//        submitButton.setMnemonic(KeyEvent.VK_ENTER);
         gamePanel.add(submitButton);
 
         resultLabel = new JLabel("");
@@ -114,10 +163,6 @@ public class MathGame extends JFrame implements ActionListener {
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
-
-
-
-
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -128,12 +173,12 @@ public class MathGame extends JFrame implements ActionListener {
                     feedbackLabel.setText("Corect. Bravo!");
                     feedbackLabel.setForeground(Color.ORANGE);
 //                    resultLabel.setText("Rezultat: " + result);
-                    num1 = random.nextInt(maxNum);
-                    num2 = random.nextInt(maxNum - num1);
+                    num1 = random.nextInt(minNum, maxNum);
+                    num2 = random.nextInt(maxNum - num1 + 1);
                     while(Integer.parseInt(num1Label.getText()) == num1 &&
                     Integer.parseInt(num2Label.getText()) == num2) {
-                        num1 = random.nextInt(maxNum);
-                        num2 = random.nextInt(maxNum - num1);
+                        num1 = random.nextInt(minNum, maxNum);
+                        num2 = random.nextInt(maxNum - num1 + 1);
                     }
                     result = num1 + num2;
                     num1Label.setText("" + num1);
@@ -150,8 +195,8 @@ public class MathGame extends JFrame implements ActionListener {
                 feedbackLabel.setForeground(Color.RED);
             }
         } else if (e.getSource() == resetButton) {
-            num1 = random.nextInt(maxNum);
-            num2 = random.nextInt(maxNum - num1);
+            num1 = random.nextInt(minNum, maxNum);
+            num2 = random.nextInt(maxNum - num1 + 1);
             result = num1 + num2;
             num1Label.setText("" + num1);
             num2Label.setText("" + num2);
@@ -162,9 +207,20 @@ public class MathGame extends JFrame implements ActionListener {
         }
     }
 
-    public static void main(String[] args) {
+    public static int getInputForMax() {
+
         int maxNum = Integer.parseInt(JOptionPane.showInputDialog("Operații până la numărul: "));
-        MathGame game = new MathGame(maxNum);
+        while(maxNum <= 1){
+            JOptionPane.showMessageDialog(null,"Numărul trebuie să fie mai mare sau egal cu 2");
+            maxNum = Integer.parseInt(JOptionPane.showInputDialog("Operații până la numărul: "));
+        }
+        return maxNum;
+    }
+
+
+    public static void main(String[] args) {
+
+        MathGame game = new MathGame(getInputForMax());
     }
 
 }
